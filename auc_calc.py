2b0da739c6647fafbb83a7aba6a93ecc5b1288f3
Agnes Folga <85518452+afolga@users.noreply.github.com>
2022-04-06 21:36:20 -0500

    Add files via upload

--- auc_calc.py
+++ auc_calc.py
@@ -22,7 +22,7 @@ with open('file1.txt') as file_obj:
             # in alternative, if you need to use the file content as numbers
             # inner_list = [int(elt.strip()) for elt in line.split(',')]
         probability_array.append(inner_list)
-file2= open('file2.txt')
+
 positive_negative=[]
 with open('file2.txt') as file_obj2:
     for line in file_obj2:
@@ -35,7 +35,7 @@ for i in range(len(probability_array)):
     if len(probability_array)==len(positive_negative):
         my_dict[probability_array[i][1]]=positive_negative[i][1]
 res = {key: val for key, val in sorted(my_dict.items(), key = lambda ele: ele[0], reverse = True)} #this sorts the values by ascending order
-
+print(res)
 tn=0# of -- in the dataset originally
 fn=0#of ++ in the dataset originally
 for x,y in res.items(): #y is the key
@@ -60,7 +60,7 @@ for x,y in res.items():
     tpr[count] = tp / (tp + fn)
     fpr[count] = fp / (tn + fp)
     count+=1
-
+print(tpr, fpr)
 
 auc_add=[0]*len(tpr)
 auc=0
@@ -72,11 +72,38 @@ for i in range(0,len(tpr)):
         auc_add[i] = tpr[i] / fpr[i]
     auc+=auc_add[i]
     #print("THIS IS AUC:"+str(auc))
-
-auc=1*np.trapz(tpr,fpr) #integrating
+#when the height or width is changing, can calculate
+auc=1*np.trapz(tpr,fpr) #integrating, probs should ask
+#have to do it by sqaures
 print(auc)
-"""
-THIS IS MY OWN CODE TO CHECK TO SEE IF IT MATCHES THE EXAMPLE!!!!!!!
-fig=px.line(x=tpr,y=fpr)
+
+
+delta_x=0
+delta_y=0
+#consider sliding window technique
+delta_x = fpr[0]
+delta_y = tpr[0]
+area=0
+num_rectangles=0
+for i in range(len(tpr)):
+    if fpr[i]!=delta_x and tpr[i]!=delta_y:
+        print(str(fpr[i])+"--"+str(delta_x)+"*"+str(delta_y))
+        area+=(fpr[i]-delta_x)*delta_y
+        print("THIS IS THE AREA:"+str(area))
+        delta_x=fpr[i]
+        delta_y=tpr[i]
+        num_rectangles+=1
+print(num_rectangles)
+    #have to check for changes in x over y
+
+
+
+#THIS IS MY OWN CODE TO CHECK TO SEE IF IT MATCHES THE EXAMPLE!!!!!!!
+fig=px.line(x=fpr,y=tpr)
 fig.show()
-"""
+
+
+
+f=open("auc.txt","w")
+f.write(str(auc))
+f.close()
